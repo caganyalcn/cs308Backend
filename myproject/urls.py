c408aa294191d799eaa0c81a67aeaf0b5738f4b2
minaerdogan <minaminaerdogan@gmail.com>
2025-03-13 21:53:53 +0300

    deneme1

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import JsonResponse
 from django.urls import include, path
 def home(request):
     return JsonResponse({"message": "Welcome to the API!"})
