da77d02bcfa3a6fcc8c25f7f0a19e9c360ae777c
caganyalcn <cagan.yalcin@sabanciuniv.edu>
2025-05-23 00:56:24 +0300

    Fixed the dump.sql file execution of it

--- myproject/settings.py
+++ myproject/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
         'HOST': 'db',  # Docker service name
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4'
         }
     }
 }
